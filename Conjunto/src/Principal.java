776aa782b236560511aa9224821031d3c666516f
Javier <javier.manmun@educa.jcyl.es>
2021-01-20 18:38:32 +0200

    Subida suma main

--- Conjunto/src/Principal.java
+++ Conjunto/src/Principal.java
@@ -1,11 +1,58 @@
+import java.util.*;
 
 public class Principal {
-
 	
-	public static void main(String[] args) {
+	static int [] crear() {
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Cuantos numeros introduces");
+		int x=teclado.nextInt();
+		int [] calcular=new int[x];
 		
 		
-		
+		for(int c=0;c<calcular.length;c++) {
+			System.out.println("Introduce el valor "+(c+1));
+			calcular[c]=teclado.nextInt();
+	}
+		return calcular;
+	}
+	
+	static int hacerSuma(int[]x) {
+		int resultado = 0;
+		for(int c=0;c<x.length;c++) {
+			resultado=resultado+x[c];
+		}
+		return resultado;
 	}
 
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		 System.out.println("dasdas");
+		 String calculo=teclado.next();
+		
+		
+		
+		switch (calculo) {
+		
+		case "suma":
+			System.out.println(hacerSuma(crear()));
+			break;
+			
+		case "resta":
+			
+			
+			break;
+			
+		case "multiplicacion":
+		
+			
+			break;
+			
+		case "division":
+			
+			
+			break;
+		}
+
+	}
 }
