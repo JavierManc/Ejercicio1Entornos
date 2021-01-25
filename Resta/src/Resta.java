164c7a1f927765e4c760cb75aaec2576bb0d33da
Andrés <andres.saludambiental@gmail.com>
2021-01-25 18:48:10 +0100

    Actualizada la resta de nuevo

--- Resta/src/Resta.java
+++ Resta/src/Resta.java
@@ -6,11 +6,11 @@ public class Resta {
 	public static int haceResta(int[] j) {
 		
 		
-		int resultado=0;
+		int resultado=j[0];
 		
-		for (int i = 0; i < j.length; i++) {
+		for (int i = 1; i < j.length; i++) {
 			
-			resultado=-(resultado-j[i]);
+			resultado=resultado-j[i];
 			
 		}
 		return resultado;
