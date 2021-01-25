b9f2c833ae7b54590b62dd5bce6bbdb5d149220d
Andrés <andres.saludambiental@gmail.com>
2021-01-25 18:24:11 +0100

    Añadida prueba junit testResta

---
+++ Resta/src/RestaTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testHaceResta() {
+		int a=1;
+		int b=1;
+		int resta[]= {a,b};
+		equals(0);
+	}
+
+}
