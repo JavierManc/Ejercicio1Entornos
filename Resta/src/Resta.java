803340a4c9424733c9f67864b196b04a477d9fc7
Andrés <andres.saludambiental@gmail.com>
2021-01-25 18:05:54 +0100

    Subida resta.

---
+++ Resta/src/Resta.java
@@ -0,0 +1,23 @@
+
+public class Resta {
+
+	
+	
+	public static int haceResta(int[] j) {
+		
+		
+		int resultado=0;
+		
+		for (int i = 0; i < j.length; i++) {
+			
+			resultado=resultado-j[i];
+			
+		}
+		return resultado;
+	}
+	
+	
+	
+	
+
+}
