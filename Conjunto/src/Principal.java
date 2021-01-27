cebf071ee4f02730c5a91a243caad6f60acd5cd8
Javier_Man <73607959+JavierManc@users.noreply.github.com>
2021-01-27 18:38:11 +0200

    Calculadora

--- Conjunto/src/Principal.java
+++ Conjunto/src/Principal.java
@@ -16,13 +16,6 @@ public class Principal {
 		return calcular;
 	}
 	
-	static int hacerSuma(int[]x) {
-		int resultado = 0;
-		for(int c=0;c<x.length;c++) {
-			resultado=resultado+x[c];
-		}
-		return resultado;
-	}
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
